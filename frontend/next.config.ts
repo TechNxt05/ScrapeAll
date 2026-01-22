1ab94ebdc8d072201e4295aae7e1a43de297a285
Amritanshu <amritanshu05yadav@gmail.com>
2026-01-22 17:56:57 +0530

    Configure Turbopack alias resolution

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
-    const path = require('path');
-    config.resolve.alias['@'] = path.join(__dirname, '.');
-    return config;
+  turbo: {
+    resolveAlias: {
+      '@': './',
+    },
   },
 };
 
